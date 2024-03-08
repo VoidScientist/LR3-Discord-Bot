dd0b8f36d91c1279db9c61d9c6d84749e41204b7
VoidScientist <louthosi59@gmail.com>
2024-03-08 09:48:50 +0100

    resolved merge conflict

--- scripts/Commands.js
+++ scripts/Commands.js
@@ -12,11 +12,8 @@ const Commands = {
     "sis" : getHotChick,
     "konami": getKonami,
     "joke": getJoke,
-<<<<<<< HEAD
-    "chuckfact" : getChuckFact
-=======
+    "chuckfact" : getChuckFact,
     "translate": getTranslation
->>>>>>> 729107a56201b483661c35e77ec1d367dee204c3
 
 };
 
@@ -111,7 +108,6 @@ async function getJoke(){
 
 }
 
-<<<<<<< HEAD
 async function getChuckFact(){
 
     const response = await fetch("https://api.chucknorris.io/jokes/random");
@@ -122,7 +118,8 @@ async function getChuckFact(){
 
     return fact.value;
 
-=======
+}
+
 async function getTranslation(args) {
 
     const languages = ["yoda", "oldenglish", "pirate", "minions", "morse", "russian-accent"]
@@ -142,7 +139,6 @@ async function getTranslation(args) {
     if (translate.error.code === 429){return translate.error.message}
 
     return translate.contents.translated;
->>>>>>> 729107a56201b483661c35e77ec1d367dee204c3
 }
 
 export default Commands;
