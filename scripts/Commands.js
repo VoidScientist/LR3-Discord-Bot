d611a5a75728cb8d1f4062d3d5c470a1d86b4113
IntScientist <153201105+raphraphra@users.noreply.github.com>
2024-03-09 14:10:13 +0100

    fixed stock 2

--- scripts/Commands.js
+++ scripts/Commands.js
@@ -223,7 +223,7 @@ async function getStockRate(args){
     
     if (data.status === "NOT_FOUND" || data.open === undefined) {return `${stock} either isn't on the stockmarket or doesn't exist.`}
 
-    return `Data for GME on ${formattedDate}: Open : ${data.open}$, Closing : ${data.close}$.` ; 
+    return `Data for ${stock} on ${formattedDate}: Open : ${data.open}$, Closing : ${data.close}$.` ; 
 
 }
 
